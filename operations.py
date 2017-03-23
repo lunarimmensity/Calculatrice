dafe5c4c490a38c2ee8ebad89bee5e032059f3cb
lunarimmensity <lunarimmensity496@gmail.com>
2017-03-23 08:06:19 +0100

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,19 @@
+import math
+
+def somme(a,b):
+    return a+b
+
+
+def soustraction(a,b):
+    return a-b
+
+
+def produit(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+
+def puissance_de_10(a,b):
+    return a*math.pow(10,b)
